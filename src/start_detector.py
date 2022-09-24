2775af8a8d42c70dff176385348bf194793eb512
Lord Asdi <42315126+LordAsdi@users.noreply.github.com>
2022-09-24 21:25:00 +0200

    Improve date time ok button detector

--- src/start_detector.py
+++ src/start_detector.py
@@ -1,3 +1,4 @@
+import time
 import numpy as np
 import cv2
 
@@ -5,9 +6,11 @@ import cv2
 class StartDetector:
     def __init__(self):
         self.start_vals = []
-        self.btn_center = [0, 0, 0]
-        self.prev_btn_center = [0, 0, 0]
-        self.avgs = 0, 0, 0, 0
+
+        self.ok_btn = False
+        self.prev_ok_btn = False
+        self.ok_btn_time = 0
+        self.date_time_border = False
 
         # Parameters
         self.lower_hue = 170
@@ -58,42 +61,72 @@ class StartDetector:
                 self.start_vals.pop(0)
 
         if date_time_ok:
-            self.prev_btn_center = self.btn_center
+            self.prev_ok_btn = self.ok_btn
 
-            frame_crop = frame[280:340, 470:570]
+            # Detect ok button
+            frame_crop = frame[280:340, 501:601]
             frame_bw = cv2.cvtColor(frame_crop, cv2.COLOR_RGB2GRAY)
             img_blur = cv2.GaussianBlur(frame_bw, (3, 3), 0)
-            sobelx = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=0, ksize=5)
             sobely = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=0, dy=1, ksize=5)
 
-            h_x, h_w, h_y, h_h, h_o = (10, 76, 9, 2, 41)
-            v_x, v_w, v_y, v_h, v_o = (4, 2, 17, 35, 80)
+            h_x, h_w, h_y, h_h, h_o = (10, 40, 9, 2, 41)
 
-            _, sobelx_th = cv2.threshold(sobelx, 64, 255, cv2.THRESH_BINARY)
             _, sobely_th = cv2.threshold(sobely, 64, 255, cv2.THRESH_BINARY)
 
-            crop1 = sobely_th[h_y:h_y + h_h, h_x:h_x + h_w]
-            avg1 = crop1.mean(axis=0).mean(axis=0)
+            crop_top = sobely_th[h_y:h_y + h_h, h_x:h_x + h_w]
+            ok_btn_top = crop_top.mean(axis=0).mean(axis=0)
 
-            crop2 = sobely_th[h_y:h_y + h_h, h_x:h_x + h_w]
-            avg2 = crop2.mean(axis=0).mean(axis=0)
+            # To check if button is highlighted
+            crop_top_rgb_avg = frame_crop[h_y + 2:h_y + 2 + h_h, h_x:h_x + h_w].mean(axis=1).mean(axis=0)
 
-            crop3 = sobelx_th[v_y:v_y + v_h, v_x:v_x + v_w]
-            avg3 = crop3.mean(axis=0).mean(axis=0)
+            crop_bot = sobely_th[h_y:h_y + h_h, h_x:h_x + h_w]
+            ok_btn_bot = crop_bot.mean(axis=0).mean(axis=0)
 
-            crop4 = sobelx_th[v_y:v_y + v_h, v_x:v_x + v_w]
-            avg4 = crop4.mean(axis=0).mean(axis=0)
+            if ok_btn_top > 127 and ok_btn_bot > 127 and \
+                    crop_top_rgb_avg[0] * 1.1 < crop_top_rgb_avg[1] and crop_top_rgb_avg[0] * 1.1 < crop_top_rgb_avg[2]:
+                self.ok_btn = True
+            else:
+                self.ok_btn = False
 
-            self.avgs = avg1, avg2, avg3, avg4
+            if not self.ok_btn and self.prev_ok_btn:
+                self.ok_btn_time = time.time()
 
-            sobelx_bgr = cv2.cvtColor(np.float32(sobelx_th), cv2.COLOR_GRAY2BGR)
-            sobely_bgr = cv2.cvtColor(np.float32(sobely_th), cv2.COLOR_GRAY2BGR)
-            cv2.rectangle(sobely_bgr, (h_x, h_y), (h_x + h_w, h_y + h_h), (255, 0, 0), 1)
-            cv2.rectangle(sobely_bgr, (h_x, h_y + h_o), (h_x + h_w, h_y + h_o + h_h), (255, 0, 0), 1)
-            cv2.rectangle(sobelx_bgr, (v_x, v_y), (v_x + v_w, v_y + v_h), (255, 0, 0), 1)
-            cv2.rectangle(sobelx_bgr, (v_x + v_o, v_y), (v_x + v_o + v_w, v_y + v_h), (255, 0, 0), 1)
+            # Draw rects for debugging
+            # sobely_bgr = cv2.cvtColor(np.float32(sobely_th), cv2.COLOR_GRAY2BGR)
+            # cv2.rectangle(sobely_bgr, (h_x, h_y), (h_x + h_w, h_y + h_h), (255, 0, 0), 1)
+            # cv2.rectangle(sobely_bgr, (h_x, h_y + h_o), (h_x + h_w, h_y + h_o + h_h), (255, 0, 0), 1)
+            # cv2.imshow("a", cv2.cvtColor(cv2.resize(sobely_bgr, (0, 0), fx=3, fy=3, interpolation=cv2.INTER_NEAREST),
+            #                              cv2.COLOR_RGB2BGR))
 
-            self.btn_center = frame[300:300 + 26, 484:484 + 6].mean(axis=0).mean(axis=0)
+            # Detect date time border
+            frame_crop = frame[223:283, 400:500]
+            frame_bw = cv2.cvtColor(frame_crop, cv2.COLOR_RGB2GRAY)
+            img_blur = cv2.GaussianBlur(frame_bw, (3, 3), 0)
+            sobely = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=0, dy=1, ksize=5)
+
+            h_x, h_w, h_y, h_h, h_o = (10, 40, 4, 2, 50)
+
+            _, sobely_th = cv2.threshold(sobely, 64, 255, cv2.THRESH_BINARY)
+
+            crop_top = sobely_th[h_y:h_y + h_h, h_x:h_x + h_w]
+            date_time_border_top = crop_top.mean(axis=0).mean(axis=0)
+
+            crop_bot = sobely_th[h_y:h_y + h_h, h_x:h_x + h_w]
+            date_time_border_bot = crop_bot.mean(axis=0).mean(axis=0)
+
+            if date_time_border_top > 127 and date_time_border_bot > 127:
+                self.date_time_border = True
+            else:
+                self.date_time_border = False
+
+            # Draw rects for debugging
+            # sobely_bgr = cv2.cvtColor(np.float32(sobely_th), cv2.COLOR_GRAY2BGR)
+            # cv2.rectangle(sobely_bgr, (h_x, h_y), (h_x + h_w, h_y + h_h), (255, 0, 0), 1)
+            # cv2.rectangle(sobely_bgr, (h_x, h_y + h_o), (h_x + h_w, h_y + h_o + h_h), (255, 0, 0), 1)
+            # cv2.imshow("b", cv2.cvtColor(cv2.resize(sobely_bgr, (0, 0), fx=3, fy=3, interpolation=cv2.INTER_NEAREST),
+            #                              cv2.COLOR_RGB2BGR))
+            # if cv2.waitKey(1) == 23:
+            #     return
 
     def check_start_button(self):
         if len(self.start_vals) < 10:
@@ -123,10 +156,7 @@ class StartDetector:
         return True
 
     def check_date_time_ok(self):
-        avg1, avg2, avg3, avg4 = self.avgs
-        if avg1 > 128 and avg2 > 128 and avg3 > 128 and avg4 > 128 and \
-                45 < self.prev_btn_center[0] < 65 and self.prev_btn_center[2] < 75 and self.prev_btn_center[1] < 70 and \
-                45 < self.btn_center[0] < 65 and self.btn_center[2] > 70 and self.btn_center[1] > 70:
+        if self.date_time_border and time.time() < self.ok_btn_time + 0.06:
             return True
 
         return False
